3058bdba5efef8c54096c4b07eebc72a45106bf3
黄敏坚 <>
2026-01-04 19:06:51 +0800

    初版

---
+++ src/getLongShortAccountRatio/index.js
@@ -0,0 +1,31 @@
+//获取合约多空持仓比
+const axios = require('axios');
+const {HttpsProxyAgent} = require('https-proxy-agent');
+const config = {
+    httpsAgent: new HttpsProxyAgent('http://127.0.0.1:7890'),
+    headers:{
+        "X-MBX-APIKEY": 'cPTBVGcDT0fjEYodmCbNJOxJXM3iX3VDKUK6OquALA6CRukNY2zzxNF3WRwhk2fV',
+    },
+}
+const moment = require('moment');
+const fs = require("fs")
+let lastTime = ''
+const main = async () => {
+    try {
+        const priceApiUrl = `https://fapi.binance.com/fapi/v1/openInterest?symbol=INJUSDT`
+        const listArr = await axios.get(priceApiUrl,config);
+        console.log('======',listArr.data)
+        if(String(listArr.data.time) !== lastTime){
+            lastTime = String(listArr.data.time)
+            const str = `${JSON.stringify(listArr.data)}====\n\r`
+            fs.appendFile("record.txt",str,(err) => {
+        
+            })
+        }
+    } catch (error) {
+        
+    }
+   
+}
+setInterval(main,60000)
+   
